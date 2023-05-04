c064fcf48b769649a09778270e72ea5a0f423aa3
Nic Hannah <nic@reask.earth>
2023-05-04 09:12:06 +0200

    Add some simple stress tests.

--- tests/test_deepcyc.py
+++ tests/test_deepcyc.py
@@ -9,7 +9,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from reaskapi.deepcyc import DeepCyc
 
-def test_florida_point():
+def generate_random_points():
 
     min_lat = 25.0
     max_lat = 30.0
@@ -24,14 +24,35 @@ def test_florida_point():
         lats.append(lat)
         lons.append(lon)
 
+    return lats, lons
+
+
+def test_florida_point():
+
     dc = DeepCyc()
 
+    lats, lons = generate_random_points()
     ret = dc.point(lats, lons)
 
     df = gpd.GeoDataFrame.from_features(ret).set_index('cell_id')
     assert len(df) == len(lats)
 
 
+def test_florida_pointep():
+
+    dc = DeepCyc()
+
+    lats, lons = generate_random_points()
+    years = [10, 20, 100, 250, 500]
+    ret = dc.pointep(lats, lons, years=years)
+
+    df = gpd.GeoDataFrame.from_features(ret).set_index('cell_id')
+    assert len(df) == len(lats)
+    assert len(df.iloc[0].windspeeds) == len(years)
+    assert sorted(df.iloc[0].windspeeds) == df.iloc[0].windspeeds
+
+
 if __name__ == '__main__':
+    test_florida_pointep()
     test_florida_point()
 
