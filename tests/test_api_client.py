312494f77c7d34cd906110db63c223962232400c
mizuguta <mizuguta@gmail.com>
2023-11-03 15:35:43 +1100

    Update test_api_client.py

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -33,3 +33,20 @@ def test_deep_cyc_config(config: ClientConfig):
             mock_session_send.call_args.args[0].url
             == f"{config.base_url if config is not None else 'https://api.reask.earth/v2'}/deepcyc/tctrack/events?radius_km=50&wind_speed_units=kph&lat=36.8&lon=-76&geometry=circle"
         )
+
+
+@pytest.mark.parametrize("config", [None, ClientConfig("https://localhost:8001/v2")])
+def test_metryc_config(config: ClientConfig):
+    with patch("reaskapi.api_client.get_access_token") as token_mock:
+        token_mock.return_value = "dummy_token"
+        mock_session_send = Mock(return_value=MockedResponse())
+        requests.Session.send = mock_session_send
+        m = Metryc(config=config)
+        m.tctrack_events(36.8, -76, "circle", radius_km=50, wind_speed_units="kph")
+        print(mock_session_send.call_args.args[0].url)
+        mock_session_send.assert_called_once()
+        assert (
+            mock_session_send.call_args.args[0].url
+            == f"{config.base_url if config is not None else 'https://api.reask.earth/v2'}/metryc/tctrack/events?radius_km=50&wind_speed_units=kph&lat=36.8&lon=-76&geometry=circle"
+        )
+
