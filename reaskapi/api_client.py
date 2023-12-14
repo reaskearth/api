08dc9619c2075a702f8b6cf4fae4f35534965555
Fabio Sato <fabio@reask.earth>
2023-12-14 17:29:59 -0300

    Added Metryc tcwind_footprint function to API client

--- reaskapi/api_client.py
+++ reaskapi/api_client.py
@@ -56,7 +56,17 @@ class ApiClient:
         self.logger.debug(f'Parameters: {params}')
 
         return self._call_api(params, f'{self.product.lower()}/tcwind/events')
+    
+    def tcwind_footprints(self, min_lat, max_lat, min_lon, max_lon, **kwargs):
 
+        params = kwargs.copy()
+        params['min_lat'] = min_lat
+        params['max_lat'] = max_lat
+        params['min_lon'] = min_lon
+        params['max_lon'] = max_lon
+        self.logger.debug(f'Parameters: {params}')
+
+        return self._call_api(params, f'{self.product.lower()}/tcwind/footprints')
 
     def tctrack_events(self, lat, lon, geometry, **kwargs):
 
